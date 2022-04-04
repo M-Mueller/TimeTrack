021c9ad1b6f22f4e788b80a4a0281bc8e2bef413
M-Mueller <M-Mueller@users.noreply.github.com>
2022-04-04 22:52:37 +0200

    Add and remove projects

--- App/src/App.fs
+++ App/src/App.fs
@@ -9,36 +9,83 @@ open Project
 
 type State =
     { currentDate: System.DateTime
+      selectedProject: ProjectName
       allProjects: ProjectName list
       projects: Map<ProjectName, WorkUnit list> }
+
+let selectableProjects (allProjects: ProjectName list) (activeProjects: Map<ProjectName, WorkUnit list>) =
+    allProjects
+    |> List.filter (fun p -> not (activeProjects.ContainsKey p))
+
+let defaultSelectedProject (allProjects: ProjectName list) (activeProjects: Map<ProjectName, WorkUnit list>) =
+    selectableProjects allProjects activeProjects
+    |> List.tryHead
+    |> Option.defaultValue ""
 
 type Msg =
     | IncrementDate
     | DecrementDate
+    | ChangeSelectedProject of ProjectName
+    | AddSelectedProject
+    | RemoveProject of ProjectName
     | UpdateWorkUnit of WorkUnitInProject
     | AppendWorkUnit of WorkUnitInProject
     | RemoveWorkUnit of WorkUnitInProject
 
 let init () =
+    let allProjects = [ "HR"; "Company Admin"; "Suite" ]
+
+    let projects =
+        Map [
+            ("HR",
+             [ { WorkUnit.hours = "4.0"
+                 comment = "Planning meeting" } ])
+        ]
+
     { currentDate = DateTime.Now
-      allProjects = [ "HR"; "Company Admin"; "Suite" ]
-      projects =
-          Map [
-              ("HR",
-               [ { hours = "4.0"
-                   comment = "Planning meeting" } ])
-          ] }
+      selectedProject = defaultSelectedProject allProjects projects
+      allProjects = allProjects
+      projects = projects }
 
 let update (msg: Msg) (state: State) : State =
     match msg with
     | IncrementDate ->
         { state with
               currentDate = state.currentDate.AddDays(1) }
+
     | DecrementDate ->
         { state with
               currentDate = state.currentDate.AddDays(-1) }
 
-    | UpdateWorkUnit (newWorkUnit) ->
+    | ChangeSelectedProject selected ->
+        { state with
+              selectedProject = selected }
+
+    | AddSelectedProject ->
+        if state.projects.ContainsKey state.selectedProject then
+            state
+        else
+            let newProjects =
+                state.projects.Add(state.selectedProject, [])
+
+            { state with
+                  projects = newProjects
+                  selectedProject = defaultSelectedProject state.allProjects newProjects }
+
+    | RemoveProject name ->
+        let newProjects = state.projects.Remove name
+
+        let newSelectedProject =
+            if String.IsNullOrWhiteSpace state.selectedProject then
+                defaultSelectedProject state.allProjects newProjects
+            else
+                state.selectedProject
+
+        { state with
+              projects = newProjects
+              selectedProject = newSelectedProject }
+
+    | UpdateWorkUnit newWorkUnit ->
         assert state.projects.ContainsKey newWorkUnit.project
 
         let oldWorkUnits = state.projects.Item newWorkUnit.project
@@ -70,6 +117,59 @@ let update (msg: Msg) (state: State) : State =
         { state with
               projects = state.projects.Add(workUnit.project, newWorkUnits) }
 
+let renderDate (dispatch: Msg -> unit) (date: System.DateTime) =
+    Html.div [
+        prop.style [ style.display.flex ]
+        prop.children [
+            Html.button [
+                prop.role "button"
+                prop.classes [ "secondary; outline" ]
+                prop.onClick (fun _ -> dispatch DecrementDate)
+                prop.text "<"
+            ]
+            Html.h1 (date.ToString("d.MM.yyyy"))
+            Html.button [
+                prop.role "button"
+                prop.classes [ "secondary; outline" ]
+                prop.onClick (fun _ -> dispatch IncrementDate)
+                prop.text ">"
+            ]
+        ]
+    ]
+
+
+let renderAddProject (dispatch: Msg -> unit) (projects: ProjectName list) (selected: ProjectName) =
+    Html.div [
+        prop.style [
+            style.display.flex
+            style.flexDirection.row
+            style.alignItems.center
+            style.marginTop (length.rem 2)
+            style.marginBottom (length.rem 2)
+        ]
+        prop.children [
+            Html.select [
+                prop.disabled projects.IsEmpty
+                prop.style [
+                    style.marginRight (length.rem 1)
+                ]
+                prop.value selected
+                prop.children (
+                    projects
+                    |> List.map (fun p -> Html.option [ prop.text p ])
+                )
+                prop.onChange (ChangeSelectedProject >> dispatch)
+            ]
+            Html.button [
+                prop.disabled (
+                    projects.IsEmpty
+                    || String.IsNullOrWhiteSpace selected
+                )
+                prop.text "Add"
+                prop.onClick (fun _ -> dispatch AddSelectedProject)
+            ]
+        ]
+    ]
 
 let renderWorkUnit (dispatch: Msg -> unit) (maxIndex: int) (projectUnit: WorkUnitInProject) =
     let dispatchChange (newUnit: WorkUnit) =
@@ -131,7 +231,26 @@ let renderProject (dispatch: Msg -> unit) (name: string) (workUnits: WorkUnit li
     Html.article [
         prop.className "card"
         prop.children [
-            yield (Html.header [ Html.h5 name ])
+            yield
+                (Html.header [
+                    prop.style [
+                        style.display.flex
+                        style.flexDirection.row
+                        style.alignItems.center
+                    ]
+                    prop.children [
+                        Html.h5 name
+                        Html.span [
+                            prop.style [ style.flexGrow 1 ]
+                        ]
+                        Html.button [
+                            prop.style [ style.color.red ]
+                            prop.className [ "pseudo" ]
+                            prop.text "X"
+                            prop.onClick (fun _ -> dispatch (RemoveProject name))
+                        ]
+                    ]
+                 ])
             yield! (List.map (renderWorkUnit dispatch maxIndex) projectUnits)
             yield
                 (renderWorkUnit
@@ -144,30 +263,15 @@ let renderProject (dispatch: Msg -> unit) (name: string) (workUnits: WorkUnit li
     ]
 
 let render (state: State) (dispatch: Msg -> unit) =
-    Html.div (
-        Html.div [
-            prop.style [ style.display.flex ]
-            prop.children [
-                Html.button [
-                    prop.role "button"
-                    prop.classes [ "secondary; outline" ]
-                    prop.onClick (fun _ -> dispatch DecrementDate)
-                    prop.text "<"
-                ]
-                Html.h1 (state.currentDate.ToString("d.MM.yyyy"))
-                Html.button [
-                    prop.role "button"
-                    prop.classes [ "secondary; outline" ]
-                    prop.onClick (fun _ -> dispatch IncrementDate)
-                    prop.text ">"
-                ]
-            ]
-        ]
-        :: (state.projects
-            |> Map.map (renderProject dispatch)
-            |> Map.toList
-            |> List.map snd)
-    )
+    Html.div [
+        yield renderDate dispatch state.currentDate
+        yield renderAddProject dispatch (selectableProjects state.allProjects state.projects) state.selectedProject
+        yield!
+            (state.projects
+             |> Map.map (renderProject dispatch)
+             |> Map.toList
+             |> List.map snd)
+    ]
 
 Program.mkSimple init update render
 |> Program.withReactSynchronous "elmish-app"
---
+++ App/src/Elements.fs
@@ -0,0 +1,22 @@
+﻿module Elements
+
+open Feliz
+
+/// An input with attached label and an optional validation error
+let labeledInput (label: string) (error: string) (props: IReactProperty list) (inputProps: IReactProperty list) =
+    Html.div [
+        yield! props
+        prop.children [
+            Html.label [
+                prop.className "labeledInput"
+                prop.children [
+                    Html.text label
+                    Html.input (prop.className "labeledInput" :: inputProps)
+                    Html.span [
+                        prop.className "labeledInput"
+                        prop.text error
+                    ]
+                ]
+            ]
+        ]
+    ]
